decbf5b742093f961b43c3bd6b7daa02a7e39c31
unknown <gadeharshada96@gmail.com>
2026-03-02 01:26:01 +0530

    Fixed module not found error

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Notification = require("../models/Notification");
+const Notification = require("../models/notification");
 const authMiddleware = require("../middleware/authMiddleware");
 const teacherOnly = require("../middleware/teacherOnly");
 
